e131e061d3c37b8a51728f91c96acc0e0c0e66a5
sagarjana00 <sagarjanaofficial@gmail.com>
2026-05-04 17:11:11 +0530

    fix preprocessor

--- preprocessor.py
+++ preprocessor.py
@@ -2,6 +2,7 @@ import re
 import pandas as pd
 
 def preprocess(data):
+    data = data.replace('\u202f', ' ')
     pattern = r'\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s(?:am|pm)?\s-\s'
     messages = re.split(pattern,data)[1:]
     dates = re.findall(pattern,data)
